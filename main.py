c90f88e2e37cf2b7ed3e491743e346daa93f8d72
Admin <mkm@yandex.ru>
2023-01-02 17:13:00 +0300

    версия с заставками

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
+import os
+import sys
+import pygame
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+class Pic(pygame.sprite.Sprite):
+    image = load_image("start_pic.png")
+    image1 = load_image("end_pic.png")
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = Pic.image1
+        self.rect = self.image.get_rect()
+        self.rect.x = -600
+        self.rect.y = 0
+
+    def update(self):
+        if self.rect.x < 0:
+            self.rect.x += 3
+
+    def titr(self):
+        self.rect.x = -600
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                    running = False
+            pic.update()
+            screen.fill('black')
+            all_sprites.draw(screen)
+            clock.tick(150)
+            pygame.display.flip()
+
 class field_object():
     def __init__(self, callable=False, other_obj=[], sym='?'):
         self._callable = callable
@@ -52,7 +92,33 @@ def ret_class(sym):
             return x
     return field_object()
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.button:
+                    pass
+        running = False
 
-field = Field('1.txt')
-field.print()
-print(field[0, 0].can_move('@'))
+pygame.init()
+size = width, height = 600, 300
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+pic = Pic(all_sprites)
+
+screen.fill('black')
+all_sprites.draw(screen)
+pic.image = Pic.image
+pic.titr()
+screen.fill('black')
+
+pic.image = Pic.image1
+pic.titr()
+
+terminate()
